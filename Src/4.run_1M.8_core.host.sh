a18544472e2d2e05d81f3c9b43f58c8b3e2f8144
Cameron Prybol <cjprybol@stanford.edu>
2016-08-13 13:01:43 -0700

    still adding container and host scripts, but needed to commit prior changes first

---
+++ Src/4.run_1M.8_core.host.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p euan,owners
+#SBATCH -n 8
+#SBATCH --mem=24G
+#SBATCH -t 2-00:00:00
+#SBATCH --export=ALL
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=cjprybol@stanford.edu
+#SBATCH -o slurm-4.quantify_transcripts_1M.8_core.out
+singularity exec singularity-manuscript.img bash 4.quantify_transcripts_1M.sh 8
