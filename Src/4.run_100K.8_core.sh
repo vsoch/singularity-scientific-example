f29d88a9f988d5aaa91a7fda976748ddd9beb4a1
Cameron Prybol <cjprybol@stanford.edu>
2016-08-12 22:46:07 -0700

    added different scripts for 4

---
+++ Src/4.run_100K.8_core.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p euan,owners
+#SBATCH -n 8
+#SBATCH --mem=24G
+#SBATCH -t 2-00:00:00
+#SBATCH --export=ALL
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=cjprybol@stanford.edu
+singularity exec singularity-manuscript.img bash 4.quantify_transcripts_100K.sh 8
---
+++ Src/4.run_10K.1_core.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p euan,owners
+#SBATCH -n 1
+#SBATCH --mem=24G
+#SBATCH -t 2-00:00:00
+#SBATCH --export=ALL
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=cjprybol@stanford.edu
+singularity exec singularity-manuscript.img bash 4.quantify_transcripts_10K.sh 1
