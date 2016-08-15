7213ae99641f9555f18641aa54ff4df052de5faa
Cameron Prybol <cjprybol@stanford.edu>
2016-08-15 16:07:58 -0700

    fixed untar which wasn't working

--- Src/1.download_data.sh
+++ Src/1.download_data.sh
@@ -9,7 +9,7 @@ if [ ! -d $FASTQ_DIR ]; then
 fi
 
 wget -P $FASTQ_DIR ftp://ngs.sanger.ac.uk/production/gencode/rgasp/RGASP1/inputdata/human_fastq/GM12878_2x75_split.tgz
-tar -xzf $FASTQ_DIR/GM12878_2x75_split.tgz
+tar --directory $FASTQ_DIR -xzf $FASTQ_DIR/GM12878_2x75_split.tgz
 
 find $FASTQ_DIR/GM12878_2x75_split -name "GM12878_2x75_rep[1-2].lane[1-3]_1.fq" -exec cat {} \; > $FASTQ_DIR/rna_1.fq && gzip $FASTQ_DIR/rna_1.fq
 find $FASTQ_DIR/GM12878_2x75_split -name "GM12878_2x75_rep[1-2].lane[1-3]_2.fq" -exec cat {} \; > $FASTQ_DIR/rna_2.fq && gzip $FASTQ_DIR/rna_2.fq
@@ -23,6 +23,6 @@ if [ ! -d $REF_DIR ]; then
 fi
 
 wget -P $REF_DIR ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_25/gencode.v25.transcripts.fa.gz
-gzip -d .$REF_DIR/gencode.v25.transcripts.fa.gz
+gzip -d $REF_DIR/gencode.v25.transcripts.fa.gz
 wget -P $REF_DIR ftp://ftp.ensembl.org/pub/release-85/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
 gzip -d $REF_DIR/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
