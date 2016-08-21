7ca88811bbe31ce2810232977099e39b3f8f886e
Cameron Prybol <cjprybol@stanford.edu>
2016-08-21 15:32:37 -0700

    RTG mapping working, moved ready to analyze trio files into box for easier download

--- Src/1.download_data.sh
+++ Src/1.download_data.sh
@@ -1,35 +1,41 @@
-#	if [ ! -d ../Data ]; then
-#		mkdir ../Data
-#	fi
-#
-#	FASTQ_DIR=../Data/Fastq
-#
-#	if [ ! -d $FASTQ_DIR ]; then
-#		mkdir $FASTQ_DIR
-#	fi
-#
-#	wget -P $FASTQ_DIR ftp://ngs.sanger.ac.uk/production/gencode/rgasp/RGASP1/inputdata/human_fastq/GM12878_2x75_split.tgz
-#	tar --directory $FASTQ_DIR -xzf $FASTQ_DIR/GM12878_2x75_split.tgz
-#
-#	find $FASTQ_DIR/GM12878_2x75_split -name "GM12878_2x75_rep[1-2].lane[1-3]_1.fq" -exec cat {} \; > $FASTQ_DIR/rna_1.fq && gzip $FASTQ_DIR/rna_1.fq
-#	find $FASTQ_DIR/GM12878_2x75_split -name "GM12878_2x75_rep[1-2].lane[1-3]_2.fq" -exec cat {} \; > $FASTQ_DIR/rna_2.fq && gzip $FASTQ_DIR/rna_2.fq
-#
-#	rm -r $FASTQ_DIR/GM12878_2x75_split
-#
+## DOWNLOAD HUMAN RNASEQ DATA
+
+if [ ! -d ../Data ]; then
+	mkdir ../Data
+fi
+
+FASTQ_DIR=../Data/Fastq
+
+if [ ! -d $FASTQ_DIR ]; then
+	mkdir $FASTQ_DIR
+fi
+
+wget -P $FASTQ_DIR ftp://ngs.sanger.ac.uk/production/gencode/rgasp/RGASP1/inputdata/human_fastq/GM12878_2x75_split.tgz
+tar --directory $FASTQ_DIR -xzf $FASTQ_DIR/GM12878_2x75_split.tgz
+
+find $FASTQ_DIR/GM12878_2x75_split -name "GM12878_2x75_rep[1-2].lane[1-3]_1.fq" -exec cat {} \; > $FASTQ_DIR/rna_1.fq && gzip $FASTQ_DIR/rna_1.fq
+find $FASTQ_DIR/GM12878_2x75_split -name "GM12878_2x75_rep[1-2].lane[1-3]_2.fq" -exec cat {} \; > $FASTQ_DIR/rna_2.fq && gzip $FASTQ_DIR/rna_2.fq
+
+rm -r $FASTQ_DIR/GM12878_2x75_split
+
+## DOWNLOAD REFERENCE GENOMES/SEQUENCES
+
 REF_DIR=../Reference
-#
-#	if [ ! -d $REF_DIR ]; then
-#		mkdir $REF_DIR
-#	fi
-#
-#	wget -P $REF_DIR ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_25/gencode.v25.transcripts.fa.gz
-#	gzip -d $REF_DIR/gencode.v25.transcripts.fa.gz
-#	wget -P $REF_DIR ftp://ftp.ensembl.org/pub/release-85/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-#	gzip -d $REF_DIR/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-#
-#	# url info for AJtrio was taken from this url
-#	# https://raw.githubusercontent.com/genome-in-a-bottle/giab_data_indexes/master/AshkenazimTrio/sequence.index.AJtrio_Illumina_2x250bps_06012016
-#
+
+if [ ! -d $REF_DIR ]; then
+	mkdir $REF_DIR
+fi
+
+wget -P $REF_DIR ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_25/gencode.v25.transcripts.fa.gz
+gzip -d $REF_DIR/gencode.v25.transcripts.fa.gz
+wget -P $REF_DIR ftp://ftp.ensembl.org/pub/release-85/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+gzip -d $REF_DIR/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+
+
+## DOWNLOAD FASTQS FOR TRIO
+
+# url info for AJtrio was taken from this url
+# https://raw.githubusercontent.com/genome-in-a-bottle/giab_data_indexes/master/AshkenazimTrio/sequence.index.AJtrio_Illumina_2x250bps_06012016
 
 FILE_LIST=$(sed '1d' $REF_DIR/sequence.index.AJtrio_Illumina_2x250bps_06012016 | awk '{print $1, $3}' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g')
 
@@ -47,3 +53,51 @@ for f in $FILE_LIST; do
 		echo "$FILENAME exists, skipping"
 	fi
 done
+
+## MERGE AND SUBSET FASTQS FOR TRIO
+
+RTG_DIR=../Data/RTG
+if [ ! -d $RTG_DIR ]; then
+	mkdir $RTG_DIR
+fi
+
+## THESE FILES HAVE BEEN MADE AVAILABLE BY FTP DOWNLOAD
+
+wget -P $RTG_DIR https://stanfordmedicine.box.com/shared/static/beky9c9u05xmljtgj4kq9iuik33xqtbq.gz -O HG002.1.10M.fastq.gz
+wget -P $RTG_DIR https://stanfordmedicine.box.com/shared/static/isod88qhvfy11d3jlxy2hc1am3axhqg9.gz -O HG002.2.10M.fastq.gz
+wget -P $RTG_DIR https://stanfordmedicine.box.com/shared/static/wu7kn19y16org4sxvp5r7nw25x3kcc18.gz -O HG003.1.10M.fastq.gz
+wget -P $RTG_DIR https://stanfordmedicine.box.com/shared/static/o2cdwpn55nuw98kmoq5o0ci67ojz4647.gz -O HG003.2.10M.fastq.gz
+wget -P $RTG_DIR https://stanfordmedicine.box.com/shared/static/sdufnqkmspj4r8sd1h1cskx5sge7t6c4.gz -O HG004.1.10M.fastq.gz
+wget -P $RTG_DIR https://stanfordmedicine.box.com/shared/static/5h4t29utrxcg9hbyen4lf6v6qid251bk.gz -O HG004.2.10M.fastq.gz
+
+# R1_REGEX=".*/D[0-9]_S[0-9]_L[0-9]+_R1_[0-9]+.fastq.gz"
+# R2_REGEX=".*/D[0-9]_S[0-9]_L[0-9]+_R2_[0-9]+.fastq.gz"
+#
+# parallel --xapply find ../Data/{1}_{2}_{3} -type f -regextype posix-egrep -regex $R1_REGEX \> /tmp/{1}.1 \
+# ::: HG002 HG003 HG004 ::: NA24385 NA24149 NA24143 ::: son father mother
+# parallel --xapply find ../Data/{1}_{2}_{3} -type f -regextype posix-egrep -regex $R2_REGEX \> /tmp/{1}.2 \
+# ::: HG002 HG003 HG004 ::: NA24385 NA24149 NA24143 ::: son father mother
+#
+# for ID in HG002 HG003 HG004
+# do
+# 	for num in 1 2
+# 	do
+# 		if [ ! -f $RTG_DIR/$ID.$num.fastq.gz ]
+# 		then
+# 			while read p
+# 			do
+# 				cat $p >> $RTG_DIR/$ID.$num.fastq.gz
+# 			done < /tmp/$ID.$num
+# 		else
+# 			echo "$RTG_DIR/$ID.$num.fastq.gz exists, skipping..."
+# 		fi
+# 	done
+# done
+#
+# for ID in HG002 HG003 HG004
+# do
+# 	for num in 1 2
+# 	do
+# 		gzip -dc $RTG_DIR/$ID.$num.fastq.gz | head -40000000 | gzip > $RTG_DIR/$ID.$num.10M.fastq.gz
+# 	done
+# done
