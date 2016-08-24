60568c5909508671dc401c092b9ca5a6d5c2133c
Cameron Prybol <cjprybol@stanford.edu>
2016-08-24 16:10:37 -0700

    retrying 4 cores, which failed because I had the wrong memory settings. and by failed I mean it requested 8 cores rather than 4

--- Src/6.run.host.sh
+++ Src/6.run.host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition euan,owners
 #SBATCH --cpus-per-task 4
-#SBATCH --mem 64G
+#SBATCH --mem 32G
 #SBATCH --time 2-00:00:00
 #SBATCH --export ALL
 #SBATCH --mail-type BEGIN,END,FAIL
