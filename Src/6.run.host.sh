8feab3e2b82d09b6a683649d6cf6a56f63dfd7bd
Cameron Prybol <cjprybol@stanford.edu>
2016-08-25 09:16:49 -0700

    merged and ran new tests for 6

--- Src/6.run.host.sh
+++ Src/6.run.host.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --partition euan,owners
-#SBATCH --cpus-per-task 12
-#SBATCH --mem 96G
+#SBATCH --partition euan
+#SBATCH --cpus-per-task 6
+#SBATCH --mem 48G
 #SBATCH --time 2-00:00:00
 #SBATCH --export ALL
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user cjprybol@stanford.edu
 #SBATCH --output slurm-6.host.out
 
-/bin/bash 6.bwa_align.sh 96 host
+/bin/bash 6.bwa_align.sh 48 host
