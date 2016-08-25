a23d1a7c03d1a01c28c2cbc74a5b4c71cff5aed0
Cameron Prybol <cjprybol@stanford.edu>
2016-08-25 09:38:44 -0700

    8*8 tests for 6

--- Src/6.run.container.sh
+++ Src/6.run.container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition euan,owners
-#SBATCH --cpus-per-task 12
-#SBATCH --mem 96G
+#SBATCH --cpus-per-task 8
+#SBATCH --mem 64G
 #SBATCH --time 2-00:00:00
 #SBATCH --export ALL
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user cjprybol@stanford.edu
 #SBATCH --output slurm-6.container.out
 
-singularity exec singularity-manuscript.img bash 6.bwa_align.sh 12 container
+singularity exec singularity-manuscript.img bash 6.bwa_align.sh 8 container
