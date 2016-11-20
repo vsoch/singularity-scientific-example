bc52613a1bdcd5917fb44d7dda0e617718efb45e
Cameron Prybol <cjprybol@users.noreply.github.com>
2016-11-20 12:47:08 -0800

    Update 7.run.sh

--- Src/7.run.sh
+++ Src/7.run.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user cjprybol@stanford.edu
 #SBATCH --output slurm-7.out
-singularity exec singularity-manuscript.img bash 7.prepare_rtg_run.sh
+singularity exec singularity-testing.img bash 7.prepare_rtg_run.sh
