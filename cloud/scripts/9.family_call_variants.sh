285e403c427eeb4922f5d110e5bc34e82f037c76
vsoch <vsochat@stanford.edu>
2017-01-21 11:50:08 -0800

    fixing last script to call variants

--- cloud/scripts/9.family_call_variants.sh
+++ cloud/scripts/9.family_call_variants.sh
@@ -14,17 +14,21 @@ fi
 REFERENCE=$DATADIR/Reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa
 RTG_DIR=$DATADIR/RTG
 MEM=$(echo "scale=2; $(free | grep 'Mem' | perl -p -e 's/^Mem: +(\d+) .+$/$1/') / 1024^2" | bc)
-MEM="$MEM"g
+MEMORY=$(echo ${MEM%.*})
+MEM="$MEMORY"g
+
+# Threads
+THREADS=$(echo "$((2 * $NUMCORES))")
 
 rtg RTG_MEM=$MEM family \
 	--output $RTG_DIR/container.trio \
 	--template $REFERENCE.sdf \
 	--machine-errors illumina \
 	--avr-model illumina-wgs.avr \
-	--threads 8 \
+	--threads $THREADS \
 	--son NA24385 \
 	--father NA24149 \
 	--mother NA24143 \
-	$RTG_DIR/container.HG002/alignments.bam \
-	$RTG_DIR/container.HG003/alignments.bam \
-	$RTG_DIR/container.HG004/alignments.bam
+	$RTG_DIR/.HG002/alignments.bam \
+	$RTG_DIR/.HG003/alignments.bam \
+	$RTG_DIR/.HG004/alignments.bam
